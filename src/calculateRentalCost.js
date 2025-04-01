8aa0f65aada70b6f0690851b7636a1a65b896885
Alya <alinataranik03@gmail.com>
2025-04-01 14:49:59 +0300

    add task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,17 +3,19 @@
  *
  * @return {number}
  */
+const dailyRate = 40;
+
 function calculateRentalCost(days) {
   // write code here
   if (days >= 7) {
-    return 40 * days - 50;
+    return dailyRate * days - 50;
   }
 
   if (days >= 3) {
-    return 40 * days - 20;
-  } else {
-    return 40 * days;
+    return dailyRate * days - 20;
   }
+
+  return dailyRate * days;
 }
 
 module.exports = calculateRentalCost;
